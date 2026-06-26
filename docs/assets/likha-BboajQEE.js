7d26b0b8446663c4bff92acd67e3e9dcef3ed257
Trinmar Boado <trinmar.boado@gmail.com>
2026-06-26 23:24:58 +0800

    build

--- docs/assets/likha-BboajQEE.js
+++
@@ -1,1 +0,0 @@
-import{aO as a}from"./vendor-M2Z5msUg.js";import{l as i}from"./index-D2996lRR.js";const r=a(({app:o})=>{o.config.globalProperties.$likha=i});export{r as default};
---
+++ docs/assets/likha-D00keWxK.js
@@ -0,0 +1,1 @@
+import{aR as a}from"./vendor-CGi2ZlZp.js";import{l as i}from"./index-D2Wgoq87.js";const r=a(({app:o})=>{o.config.globalProperties.$likha=i});export{r as default};
