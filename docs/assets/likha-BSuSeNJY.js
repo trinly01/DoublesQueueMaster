c77d10b022f78732c13d4eca4b797ad3d957f145
Trinmar Boado <trinmar.boado@gmail.com>
2026-08-12 08:43:03 +0800

    build: PWA with auth cleanup and recent clubs first-login fix

---
+++ docs/assets/likha-BSuSeNJY.js
@@ -0,0 +1,1 @@
+import{aT as a}from"./vendor-qpRQqlV-.js";import{l as i}from"./index-M6VuuFty.js";const r=a(({app:o})=>{o.config.globalProperties.$likha=i});export{r as default};
--- docs/assets/likha-CZ7yBJG8.js
+++
@@ -1,1 +0,0 @@
-import{aT as a}from"./vendor-D7qHfGhj.js";import{l as i}from"./index-CBEpMA4h.js";const r=a(({app:o})=>{o.config.globalProperties.$likha=i});export{r as default};
