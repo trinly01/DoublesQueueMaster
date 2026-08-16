dacf2e584c444bd98335a10031c109f7d351034e
Trinmar Boado <trinmar.boado@gmail.com>
2026-08-16 16:20:21 +0800

    build: PWA production build

--- docs/assets/likha-D9ci33TG.js
+++
@@ -1,1 +0,0 @@
-import{aW as t,P as i}from"./vendor-sZw2WCvs.js";import{l as a}from"./index-qUSPGNAs.js";const l=t(async({app:o})=>{if(o.config.globalProperties.$likha=a,i.has("dink-auth"))try{await a.refresh()}catch{console.warn("[Boot] Proactive token refresh failed")}});export{l as default};
